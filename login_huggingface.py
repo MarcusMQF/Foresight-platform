cfb65b33e5ebc67df42f52a5e718aaae138492b7
Marcus <marcusmah6969@gmail.com>
2025-05-27 11:59:54 +0800

    Update .gitignore

--- login_huggingface.py
+++
@@ -1,7 +0,0 @@
-from huggingface_hub import login
-import getpass
-
-print("Please enter your Hugging Face token (create one at https://huggingface.co/settings/tokens):")
-token = getpass.getpass()
-login(token=token)
-print("Login successful. You can now use Hugging Face models.") 
